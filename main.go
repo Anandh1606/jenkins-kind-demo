15d0990f4d027f493ac2c0ebff9d9057b5a2e4f6
Anandh1606 <anandhofficial16@gmail.com>
2025-12-16 23:46:16 +0530

    Update main.go

--- main.go
+++ main.go
@@ -8,5 +8,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 func main() {
     http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+    http.ListenAndServe(":11000", nil)
 }
